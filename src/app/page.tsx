a8b0b4f9164f48ae5b0925a37ca2dbe559d34029
Jorge Assaf <jorgeassaf160@gmail.com>
2023-05-17 11:07:40 -0600

    fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Form from './components/Form'
 
 export default async function Home() {
   return (
-    <main >
+    <main>
       <div className='imagen h-screen'>
         <div className=' flex flex-col items-center justify-center h-full'>
           <div className='container mx-auto md:w-2/6 w-11/12 '>
             <h1 className='md:text-4xl text-3xl font-bold text-center text-white'>
-              Liminal Spaces Images Generator
+              Liminal Space Images Generator
             </h1>
             <Form />
           </div>
