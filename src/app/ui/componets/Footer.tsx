f09e7f16085367d4610fc7f6b23f889c74bc1884
Jorge Assaf <jorgeassaf160@gmail.com>
2023-05-17 18:08:06 -0600

    color

--- src/app/ui/componets/Footer.tsx
+++ src/app/ui/componets/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className='my-16 mx-auto text-white'>
+    <footer className='my-16 mx-auto text-neutral-400'>
       Hecho con <span>Next.js</span> por{' '}
       <a className='hover:underline' href='https://assafdev.xyz'>
         Jorge Assaf
